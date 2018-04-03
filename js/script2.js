2909986e73c51a911df8e87e8728a7c9c8d146ae
Timothy Keaveny <timothykeaveny@ool-ad02013b.dyn.optonline.net>
2018-04-03 09:08:36 -0400

    Fixed issue with event listeners see script2.js

--- js/script2.js
+++ js/script2.js
@@ -1,3 +1,5 @@
+const playerOne = '';
+const playerTwo = '';
 const columns = 8;
 const rows = 8;
 const squareSize = 50;
@@ -9,15 +11,29 @@ const aircraftCarrier = 3;
 const vessels = [cruiser, cruiser, cruiser, battleship, battleship, aircraftCarrier];
 let playerOneScore = 0;
 let playerTwoScore = 0;
-let playerTurn = 0;
-
+let playerTurn = 1;
 const fieldOne = $('.fieldOne');
 const fieldTwo = $('.fieldTwo');
+const winner = '';
+
+//a simple function to add two values
+function add(a, b) {
+  return a + b;
+};
+
+//total points a plaayer must score to win the game
+const winningScore = vessels.reduce(add, 0);
 
 //fnction to turn a grid square grey
 function turnGrey() {
   $(this).css('background-color', 'grey');
 };
+
+//function to turn a gird square red for a positive hit
+function hitShip() {
+  alert('Direct hit!');
+  $(this).css('background-color', 'red');
+}
 
 //the below 'for' loops are used to create divs for the grid
 //I'll eventually combined the two loops into a single function
@@ -25,7 +41,8 @@ function createFieldOne() {
   for (let i = 0; i < 64; i++) {
     let square = $('<div></div>');
     square.addClass('square');
-    square.attr('id', 'sq' + i);
+    square.addClass('f1');
+    square.attr('id', 'sqOne' + i);
     square.click(turnGrey);
     $(squareArray).append(square);
     $('.fieldOne').append(square);
@@ -39,7 +56,8 @@ function createFieldTwo() {
   for (let i = 0; i < 64; i++) {
     const square = $('<div></div>');
     square.addClass('square');
-    square.attr('id', 'sqT' + i);
+    square.addClass('f2');
+    square.attr('id', 'sqTwo' + i);
     square.click(turnGrey);
     $(squareArray).append(square);
     $('.fieldTwo').append(square);
@@ -47,51 +65,34 @@ function createFieldTwo() {
 }
 createFieldTwo();
 
-//function to turn a gird square red for a positive hit
-function hitShip() {
-  alert('Direct hit!');
-  $(this).css('background-color', 'red');
-}
-/*
-//this is a place-holder event handler
-//currently turns square grey to represent misses.
-function emptySquare() {
-  $('.square').click(turnGrey);
-}
-*/
-//the below function is for successful strikes on a ship.
-//this prevents the default behavior from the click event (grey square for misses).
-//instead it changes the color to red and increments player's score.
-function occupiedSquare() {
-  $('.square').click(function(e) {
-    e.preventDefault();
-    if ($(this).hasClass('ship')) {
-      hitShip();
-      playerTwoScore += 1;
-      $('.second').html(playerTwoScore);
-    }
-    else if ($(this).hasClass('ship-two')) {
-      hitShip();
-      playerOneScore += 1;
-      $('.first').html(playerOneScore);
-    }
-  })
-};
 //place shiprandomly on the grid horizontally
 //by default, it places the ships in the first field.
 //this is because the program finds the first instance of 'sq*'
 function placeHorizontal(n, boat) {
   for (let i = 0; i < boat; i++) {
-    let square = $('#sq' + (n + i));
+    let square = $('#sqOne' + (n + i));
     $(square).addClass('ship');
+    square.click(function(e) {
+      e.preventDefault();
+      hitShip();
+      playerTwoScore += 1;
+      $('.second').html(playerTwoScore);
+    })
   }
 }
+
 //seperate function to place ship in second field
 //needed due to field two's unique ID's
 function placeHorizontalPlayerTwo(n, boat) {
   for (let i = 0; i < boat; i++) {
-    let square = $('#sqT' + (n + i));
+    let square = $('#sqTwo' + (n + i));
     $(square).addClass('ship-two');
+    square.click(function(e) {
+      e.preventDefault();
+      hitShip();
+      playerOneScore += 1;
+      $('.second').html(playerOneScore);
+    })
   }
 }
 
@@ -100,8 +101,14 @@ function placeHorizontalPlayerTwo(n, boat) {
 //this is because the program finds the first instance of 'sq*'
 function placeVertical(n, boat) {
   for (let i = 0; i < boat * 8; i+=8) {
-    let square = $('#sq' + (n + i));
+    let square = $('#sqOne' + (n + i));
     $(square).addClass('ship');
+    square.click(function(e) {
+      e.preventDefault();
+      hitShip();
+      playerTwoScore += 1;
+      $('.second').html(playerTwoScore);
+    })
   }
 }
 
@@ -109,8 +116,14 @@ function placeVertical(n, boat) {
 //needed due to field two's unique ID's
 function placeVerticalPlayerTwo(n, boat) {
   for (let i = 0; i < boat * 8; i+=8) {
-    let square = $('#sqT' + (n + i));
+    let square = $('#sqTwo' + (n + i));
     $(square).addClass('ship-two');
+    square.click(function(e) {
+      e.preventDefault();
+      hitShip();
+      playerOneScore += 1;
+      $('.second').html(playerOneScore);
+    })
   }
 }
 
@@ -160,7 +173,6 @@ function isShipInRangeVPlayerTwo(boat) {
   }
 }
 
-
 //The setBoard function randomly places the-
 //ships on the board for both players.
 function setBoard() {
@@ -175,83 +187,64 @@ function setBoard() {
     }
   }
 };
-/*
-//This will be the primary function to start and play the game.
-function whoseTurn() {
-  //let playerTurn = 0;
-  playerTurn += 1;
-  if (playerTurn % 2 === 0) {
-    console.log('test1');
-    console.log(playerTurn);
-    //alert('Player 2, Your turn. Choose a target!');
-    $('h1').html('Player 2, Your turn. Choose a target!');
-    $('.square').on('click', function(e) {
-      if($(e.target).is('.square')) {
-        //playerTurn += 1;
-        whoseTurn();
-        //switch players
-      }
-    })
+
+function checkWinner() {
+  if (playerOneScore === winningScore) {
+    winner = PlayerOne;
   }
-  else if (playerTurn % 2 !== 0) {
-    console.log('test2');
-    console.log(playerTurn);
-    //alert('Player 1 Your turn. Choose a target!');
-    $('h1').html('Player 1, Your turn. Choose a target!');
-    console.log('test3');
-    console.log(playerTurn);
-    $('.square').on('click', function(e) {
-      if($(e.target).is('.square')) {
-        //playerTurn += 1;
-        whoseTurn();
-        //switch players
-      }
-    })
+  else if (playerTwoScore === winningScore) {
+    winner = PlayerTwo;
+  }
+  else {
+    return;
   }
 };
-*/
-/*
+
+//function to switch players, check for scored point and for winner
 function whoseTurn() {
-  playerTurn += 1;
-  if (playerTurn % 2 === 0) {
-    $('h1').html('Player 2, Your turn. Choose a target!');
-    $('.square').on('click', function(e) {
-      if($(e.target).is('.square')) {
-        //square.off('click', '.square', emptySquare);
-        //square.off('click', '.square', occupiedSquare);
-        console.log('test1');
-        whoseTurn();
-      }
-    })
-  }
-  else if (playerTurn % 2 !== 0) {
+  if (playerTurn % 2 !== 0) {
     $('h1').html('Player 1, Your turn. Choose a target!');
-    $('.square').on('click', function(e) {
-      if($(e.target).is('.square')) {
-        //square.off('click', '.square',emptySquare,);
-        //square.removeEventListener('click', occupiedSquare);
-        console.log('test2');
+    if ($(this).hasClass('f1')) {
+      if ($(this).hasClass('ship')) {
+        //hitShip();
+        //playerOneScore += 1;
+        //$('.first').html(playerOneScore);
+        checkWinner();
         whoseTurn();
       }
-    })
+      else {
+        whoseTurn();
+      }
+    }
+    else {
+      whoseTurn();
+    }
   }
-}
-*/
-
-
-
-
-
-
-
-
+  else {
+    //(playerTurn % 2 === 0)
+    $('h1').html('Player 2, Your turn. Choose a target!');
+    if ($(this).hasClass('f2')) {
+      if ($(this).hasClass('ship')) {
+        //hitShip();
+        //playerTwoScore += 1;
+        //$('.second').html(playerTwoScore);
+        checkWinner();
+        whoseTurn();
+      }
+      else {
+        whoseTurn();
+      }
+    }
+    else {
+      whoseTurn();
+    }
+    }
+  //}
+};
 
 function startGame() {
-  //emptySquare();
-  //occupiedSquare();
   setBoard();
   whoseTurn();
 };
-
 
 startGame();
